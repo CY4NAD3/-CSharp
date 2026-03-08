b551e822cdb503c3e0a3dc7ffc8cfc081634e908
Md. Azraful Alam <24-57476-2@student.aiub.edu>
2026-03-08 15:21:42 +0600

    commit

---
+++ Bro_code/Constants/Constants/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constants = immutable values which are known at compile time 
+            //and do not change for the life of the program
+
+            const double pi = 3.13159;
+
+            //pi = 420;
+
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bro_code/MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args) // main method
+        {   //main method body
+            Console.WriteLine("I like pizza!");
+            Console.WriteLine("It's really good!");
+            Console.Beep();
+
+            Console.Write("Hey!!");
+            Console.WriteLine("Hello!!!"); //String literal
+
+            //This is a comment
+            /* This is a multi-line comment
+             * It can span multiple lines
+             * It is useful for longer explanations or documentation
+             */
+
+            //Escape sequences
+            Console.WriteLine("\tBroCode"); 
+            Console.WriteLine("Bro\bCode"); 
+            Console.WriteLine("\tBro\bCode"); 
+            Console.WriteLine("Bro\nCode");
+            Console.WriteLine("Bro\\Code");
+
+            Console.ReadKey();
+        }
+    }
+}
