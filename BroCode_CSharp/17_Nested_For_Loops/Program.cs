6dd4840b79bb5bec0e9ee7ef7ec0ba293d27c55a
Md. Azraful Alam <24-57476-2@student.aiub.edu>
2026-03-11 01:57:43 +0600

    commit

--- BroCode_CSharp/17_Nested_For_Loops/Program.cs
+++ BroCode_CSharp/17_Nested_For_Loops/Program.cs
@@ -10,6 +10,48 @@ namespace _17_Nested_For_Loops
     {
         static void Main(string[] args)
         {
+            // Nested for loops = one loop inside another loop
+            //                    Uses vary, Used a lot in sorting algos
+
+            Console.Write("How many rows? :");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("How many columns? :");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            Console.Write("What symbol or character do you want to use? :");
+            String symbol = Console.ReadLine();
+
+            Console.WriteLine();
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // Normal Triangle
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //Alternative code Normal Triangle
+            for (int i = rows; i > 0; i--) {
+                for (int j = columns; j >= i; j--)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
         }
     }
 }
