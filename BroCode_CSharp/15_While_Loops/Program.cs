7f7674bdf5e5e645d44e93db74cde95e33f22475
Md. Azraful Alam <24-57476-2@student.aiub.edu>
2026-03-11 00:07:53 +0600

    commit

--- BroCode_CSharp/15_While_Loops/Program.cs
+++ BroCode_CSharp/15_While_Loops/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,22 @@ namespace _15_While_Loops
     {
         static void Main(string[] args)
         {
+            // while loop = repeats a block of code while a specified condition is true
+
+            String name = "";
+
+            while (name == "") {
+                Console.Write("Enter a name: ");
+                name = Console.ReadLine();
+            }
+
+            //while (1 == 1) {
+            //    Console.Write("Infinite loop");   
+            //}
+
+            Console.WriteLine("Hello " + name);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ BroCode_CSharp/16_For_Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loops = execute a block of code a FINITE number of times
+
+            for(int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+            for(int i = 10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
