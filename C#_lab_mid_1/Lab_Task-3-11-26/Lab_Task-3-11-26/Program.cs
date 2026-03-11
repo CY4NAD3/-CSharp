6fbe9f534ce5a2f8cd0f285ea2a783abfe4b77a4
Md. Azraful Alam <24-57476-2@student.aiub.edu>
2026-03-11 14:51:45 +0600

    commit

---
+++ C#_lab_mid_1/Lab_Task-3-11-26/Lab_Task-3-11-26/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Task_3_11_26
+{
+    class FoodItem
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        // Virtual method
+        public virtual double CalculateBill()
+        {
+            return Price * Quantity;
+        }
+
+        public void DisplayItem()
+        {
+            Console.WriteLine("Food Item: " + Name);
+            Console.WriteLine("Price: " + Price);
+            Console.WriteLine("Quantity: " + Quantity);
+        }
+    }
+
+    class Pizza : FoodItem
+    {
+        public override double CalculateBill()
+        {
+            double total = (Price * Quantity) + 50; // service charge
+            return total;
+        }
+    }
+
+    class Pasta : FoodItem
+    {
+        public override double CalculateBill()
+        {
+            double total = (Price * Quantity) + 30; // packaging charge
+
+            if (Quantity > 20)
+            {
+                total = total - (total * 0.05); // 5% discount
+            }
+
+            return total;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Select Food Item:");
+            Console.WriteLine("1. Pizza");
+            Console.WriteLine("2. Pasta");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Price: ");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+
+            FoodItem item;
+
+            if (choice == 1)
+            {
+                item = new Pizza();
+                item.Name = "Pizza";
+            }
+            else
+            {
+                item = new Pasta();
+                item.Name = "Pasta";
+            }
+
+            item.Price = price;
+            item.Quantity = quantity;
+
+            Console.WriteLine("\nOrder Details:");
+            item.DisplayItem();
+
+            double totalBill = item.CalculateBill();
+
+            Console.WriteLine("Total Bill: " + totalBill);
+        }
+    }
+}
