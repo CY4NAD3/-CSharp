f22cc686de69b51705a0a30ca19c28c1a0c162ec
Md. Azraful Alam <24-57476-2@student.aiub.edu>
2026-03-19 16:57:59 +0600

    linux mods

--- BroCode_CSharp/17_Nested_For_Loops/Program.cs
+++ BroCode_CSharp/17_Nested_For_Loops/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -31,7 +33,7 @@ namespace _17_Nested_For_Loops
             }
             Console.WriteLine();
 
-            // Normal Triangle
+            // Right angle left side
             for (int i = 0; i < rows; i++) {
                 for (int j = 0; j <= i; j++)
                 {
@@ -41,9 +43,25 @@ namespace _17_Nested_For_Loops
             }
             Console.WriteLine();
 
-            //Alternative code Normal Triangle
+            // Inverse right angle left side
             for (int i = rows; i > 0; i--) {
-                for (int j = columns; j >= i; j--)
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            
+            // Right angle right side
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int k = rows - i; k > 0; k--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
                 {
                     Console.Write(symbol);
                 }
@@ -51,6 +69,74 @@ namespace _17_Nested_For_Loops
             }
             Console.WriteLine();
 
+            // Inverse right angle right side
+            for (int i = rows; i > 0; i--)
+            {
+                for (int k = rows - i; k > 0 ; k--)
+                {
+                    Console.Write(" ");     
+                }
+                for (int j = i ; j > 0 ; j--)
+                {
+                    Console.Write(symbol);  
+                }
+                Console.WriteLine();    
+            }
+            Console.WriteLine();
+            
+            for (int i = 1; i <= rows; i++)
+            {
+                Console.Write(new String(' ', rows - i));
+                Console.Write(new String(Convert.ToChar(symbol), 2 * i - 1));
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int k = 0; k < rows - i; k++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < 2 * i - 1 ; j++)
+                {
+                    Console.Write(symbol);  
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //diamond
+            for (int i = 1; i < rows; i++)
+            {
+                Console.Write(new String(' ', rows - i));
+                Console.Write(new String(Convert.ToChar(symbol), 2 * i - 1));
+                Console.WriteLine();
+            }for (int i = rows; i > 0; i--)
+            {
+                Console.Write(new String(' ', rows - i));
+                Console.Write(new String(Convert.ToChar(symbol), 2 * i - 1));
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            
+            //XO rectangle
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O"); 
+                    }
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
---
+++ BroCode_CSharp/18_Number_Guessing_Game/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_Number_Guessing_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+
+            Console.ReadKey();
+        }
+    }
+}
